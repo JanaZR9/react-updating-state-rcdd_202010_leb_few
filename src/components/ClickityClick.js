adecfef1792db6c125b1bf248bee286c87d4ecbc
Jana Zrayka <janjoune.97@hotmail.com>
2021-01-07 21:50:22 +0000

    Automatically backed up by Learn

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,21 @@
 // Code ClickityClick Component Here
+import React, {Component} from 'react';
+
+export default class ClickityClick extends Component{
+   constructor (){
+     super();
+     this.state={
+       hasbeenClicked:false
+     };
+   }
+   
+   handleClick = () => { 
+     this.setState(previousState => {
+       return {
+         hasbeenClicked: !previousState.hasbeenClicked
+       }
+     })
+   }
+   
+   
+}
